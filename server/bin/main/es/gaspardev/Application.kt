0cc4b6e8c8627dbf0b6e10ee2476f5fc84fb4ad4
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-09-24 12:53:44 +0200

    dataBase docker deploy preparations

--- server/bin/main/es/gaspardev/Application.kt
+++ server/bin/main/es/gaspardev/Application.kt
@@ -3,18 +3,34 @@ package es.gaspardev
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import java.io.*
 
 fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(Netty, port = 8080) {
+        // Inicialización de la aplicación
+        environment.monitor.subscribe(ApplicationStarted) {
+            // Ejecutar el comando docker cuando el servidor inicie
+            startSurrealDB()
+        }
+
+    }.start(wait = true)
 }
 
-fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
-        }
+fun startSurrealDB() {
+    val command =
+        listOf("docker", "run", "--rm", "--pull", "always", "-p", "8000:8000", "surrealdb/surrealdb:latest", "start")
+
+    try {
+        val process = ProcessBuilder(command)
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start()
+
+        println("SurrealDB está arrancando...")
+        process.waitFor() // Espera que el comando termine, opcional
+        println("SurrealDB ha iniciado.")
+    } catch (e: IOException) {
+        e.printStackTrace()
+        println("Error al iniciar SurrealDB.")
     }
 }
